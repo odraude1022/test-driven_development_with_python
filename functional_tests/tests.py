abbb6c394899dfbe80b2da886c6a36b33e65d436
Eduardo Iglesias <eriglesias@ufl.edu>
2019-10-16 18:03:02 -0400

    add new url for adding items to existing list. functional tests now pass

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,8 +75,6 @@ class NewVisitorTest(LiveServerTestCase):
 
     # Satisfied, she goes back to sleep
 
-    self.fail('Finish the test!')
-
   def test_multiple_users_can_start_lists_at_different_urls(self):
     # Edith starts a new to-do list
     self.browser.get(self.live_server_url)
