9d27c6bb9fd1eaf44e878de4adcb1b353791e9da
Eduardo Iglesias <eriglesias@ufl.edu>
2019-11-07 13:18:45 -0500

    use new form in home_page, simplify tests. breaks functional tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -12,6 +13,10 @@ class HomePageTest(TestCase):
   def test_uses_home_template(self):
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
+  
+  def test_home_page_uses_item_form(self):
+    response = self.client.get('/')
+    self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
 
