49bf34bdc1b82cb33fa1e22cb9ac0d592379ecbb
Eduardo Iglesias <eriglesias@ufl.edu>
2019-12-03 20:30:53 -0500

    link authentication backend to views and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.core.mail import send_mail
 from django.urls import reverse
 from django.shortcuts import redirect, render
@@ -26,4 +26,7 @@ def send_login_email(request):
   return redirect('/')
 
 def login(request):
+  user = auth.authenticate(uid=request.GET.get('token'))
+  if user:
+    auth.login(request, user)
   return redirect('/')
